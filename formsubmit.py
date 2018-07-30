744b8025f21b520b6b1219f24ffa6a81d824a89f
XCup <335908665@qq.com>
2018-07-30 20:38:27 +0800

    123

--- formsubmit.py
+++ formsubmit.py
@@ -4,25 +4,6 @@ import tornado.web
 import subprocess
 import tornado.ioloop
 import simplejson as json
-class BaseHandler(tornado.web.RequestHandler):
-
-    def set_default_headers(self):
-        print("setting headers!!!")
-        self.set_header("Access-Control-Allow-Origin", "*") # 这个地方可以写域名
-        self.set_header("Access-Control-Allow-Headers", "x-requested-with")
-        self.set_header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
-
-    def post(self):
-        self.write('some post')
-
-    def get(self):
-        self.write('some get')
-
-    def options(self):
-        # no body
-        self.set_status(204)
-        self.finish()
-
 class indexHandler(tornado.web.RequestHandler):
     def set_default_headers(self):
         print("setting headers!!!")
