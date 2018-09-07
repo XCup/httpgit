29dbe31bf7fcc2893261ffd4513dcc65b48aec62
Your Name <335908665 name = XCup>
2018-09-07 11:46:40 +0800

    123321

--- pachong1.py
+++ pachong1.py
@@ -1,15 +1,15 @@
 import requests
 import re
 from bs4 import BeautifulSoup
-r=requests.get('https://github.com/XCup/gitpushscript')
+r=requests.get('http://bns.qq.com/cp/a20180619ggtrip/index.htm')
 #print(r.text)
 
 soup = BeautifulSoup(r.text,'lxml')
-find = str(soup.find_all('span'))
+find = str(soup.find_all('tbody'))
 #print(find)
 key = find
-p1 = r"(?<=title=\").*?(?=\">)"
-pattern1 = re.findall('(?<=title=\").*?(?=\">)',key,re.S)
+#p1 = r"(?<=title=\").*?(?=\">)"
+pattern1 = re.findall('(?<=tbody id=).*?(?=body>)',key,re.S)
 #pattern1 = re.compile(p1)
 #matcher1 = re.search(pattern1,key)
 #print(matcher1.group())
