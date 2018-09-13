c4148f65668c33674b84b043bbcf708cf3c0a1bf
Your Name <335908665 name = XCup>
2018-09-13 18:04:43 +0800

    111

--- orm/schema.py
+++ orm/schema.py
@@ -1,12 +1,28 @@
+import tornado
+import tornado.web
+import tornado.ioloop
 import mysqlPy,asyncio
 from models import entity
+class MainHandler(tornado.web.RequestHandler):
+    def get(self, *args, **kwargs):
+        self.write("hello")
+    def post(self, *args, **kwargs):
+        message=self.get_argument('commit')
+        type = 233
+        async def test(loop):
+            await mysqlPy.create_pool(loop,user='bm',password='bm!@#123',db='dev')
+            u = entity(data=message)
+            await u.save()
 
-async def test(loop):
-    await mysqlPy.create_pool(loop,user='bm',password='bm!@#123',db='dev')
-    u = entity(data='Test1')
-    await u.save()
 
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(asyncio.wait([test(loop)]))
+        loop.run_forever()
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(asyncio.wait([test(loop)]))
-loop.run_forever()
+if __name__ == '__main__':
+    app = tornado.web.Application ([
+        ('/save', MainHandler),
+    ]
+    )
+    app.listen (8887)
+    tornado.ioloop.IOLoop.instance ().start ()
--- zhengze.py
+++ zhengze.py
@@ -1,1 +1,4 @@
-print(type(user))
+L = [1,2,3]
+num = 0
+for num in L:
+    print(num)
